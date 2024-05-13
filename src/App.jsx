995b42bdaffd04fc3307e99c14401407c860ac07
Mikhail Rodionov <misha25025.su@gmail.com>
2024-05-13 23:09:15 +0300

    removed main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { createRoot } from "react-dom/client";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App =() => {
   const [count, setCount] = useState(0)
 
   return (
@@ -31,5 +33,9 @@ function App() {
     </>
   )
 }
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
 
 export default App
